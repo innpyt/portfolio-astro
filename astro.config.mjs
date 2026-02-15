b9479a764a75af203b46607d924450333b5b2e2b
morfe1 <innpyt@gmail.com>
2026-02-15 13:00:03 +0100

    add astro icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
 
 export default defineConfig({
+    integrations: [icon()],
     vite: {
         plugins: [tailwindcss()],
     },
