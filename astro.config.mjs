ef908f7eb181f68bf5eee2f86e061925a79bcc25
morfe1 <innpyt@gmail.com>
2026-02-17 21:23:02 +0100

    update quality, change all .md files to .mdx, use <Image>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
+import mdx from '@astrojs/mdx';
 // import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-    integrations: [icon()],
+    integrations: [icon(), mdx()],
     vite: {
         plugins: [tailwindcss()],
     },
